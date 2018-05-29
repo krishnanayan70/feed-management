b88a2e7c332d9e16f06fe0c339b3a30dafc12873
Krishna Nayan Choudhary <krishnanayan70@gmail.com>
2018-05-29 10:03:13 +0530

    initial project structure

---
+++ src/main/java/com/stringsai/feedmanagement/FeedmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.stringsai.feedmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedmanagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stringsai/feedmanagement/FeedmanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stringsai.feedmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedmanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
